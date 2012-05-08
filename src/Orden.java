a809425801abc59633d7b82a318682d510d55ae1
kbaudio <amigodelpc@gmail.com>
2012-05-08 00:52:33 -0400

    avance1

--- src/Orden.java
+++ src/Orden.java
@@ -1,18 +1,29 @@
 import javax.swing.*;
 import java.net.*;
+import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Font;
 import java.io.*;
 public class Orden extends JFrame{
 
 // componentes de panel	
 	JPanel panel = new JPanel();
 	JButton boton1 = new JButton("Ir a pagina !");
-	JLabel etiqueta1 = new JLabel("Ir a Chrome");
-	private String image1 = "/images/fondo.jpg";
-
+	private String image1 = "/home/kala/git/Varios/Images/fondo.jpg";
 	ImageIcon ico = new ImageIcon(image1);
+	JLabel texto1 = new JLabel("Ir a Chrome");
+	JLabel etiqueta = new JLabel();
 	
-	public Orden(){
-panel.add(etiqueta1);
+	Font fuente = new Font("Helvetica", Font.ITALIC, 20);
+	
+public Orden(){
+etiqueta.setIcon(ico);
+texto1.setForeground(Color.WHITE);
+texto1.setFont(fuente);
+texto1.setSize(140, 20);
+texto1.setLocation(120, 30);
+etiqueta.add(texto1);
+panel.add(etiqueta);
 setContentPane(panel);
 setSize(400,400);
 setVisible(true);
@@ -22,6 +33,13 @@ close();
 
 public void close(){
 	
+addWindowListener(new WindowAdapter(){
+	
+	public void windowClosing(WindowEvent e){
+		System.exit(0);
+	}
+	
+});
 }
 	
 	public static void abrir() throws URISyntaxException {
