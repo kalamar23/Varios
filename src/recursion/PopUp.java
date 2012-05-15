e837aa55dc09b45862484c7b95533fb2ba359ce1
kbaudio <amigodelpc@gmail.com>
2012-05-14 20:20:53 -0400

    incompleto6

--- src/recursion/PopUp.java
+++ src/recursion/PopUp.java
@@ -1,4 +1,5 @@
 package recursion;
+
 // En esta clase se puede apreciar el beneficio de clases anidadas 
 import java.awt.Graphics;
 import java.awt.Image;
@@ -7,6 +8,7 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JButton;
@@ -14,104 +16,117 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 
 public class PopUp extends JFrame {
-	
-	PopUp(){
-		setSize(404,171);
+
+	PopUp() {
+		setSize(404, 171);
 		setUndecorated(true);
 		add(new Entrada());
 		setLocationRelativeTo(null);
-		setVisible(true);		
+		setVisible(true);
+
 	}
-	
-	
-	
-	
+
 	class Entrada extends JPanel implements ActionListener {
-		int[] temp = new int[15];
+		byte index = 1;
+		int[] temp = new int[index];
 		private byte count = 0;
 
 		JButton aceptar = new JButton("Aceptar");
+		JButton cerrar = new JButton();
 		JTextField input1 = new JTextField();
+		ImageIcon icerrar = new ImageIcon("/home/kala/git/Varios/Images/botonx.jpg");
 		Image popup = null;
-			
-			
-			Entrada(){
+
+		Entrada() {
 			setLayout(null);
-		add(aceptar);
-		aceptar.setBounds(300,130, 100, 30);
-		aceptar.addActionListener(this);
-		input1.setBounds(120, 90, 180, 30);
-		add(input1);
-			popup = Toolkit.getDefaultToolkit().getImage("/home/kala/git/Varios/Images/popup.jpg");
+			botones(aceptar,null,300, 130, 100, 30);
+			botones(cerrar,icerrar,5,130,77,41);
+			input1.setBounds(120, 90, 180, 30);
+			add(input1);
+			popup = Toolkit.getDefaultToolkit().getImage(
+					"/home/kala/git/Varios/Images/popup.jpg");
 			MediaTracker mt = new MediaTracker(this);
 			mt.addImage(popup, 0);
-			try{
+			try {
 				mt.waitForAll();
-			}catch(InterruptedException e){
+			} catch (InterruptedException e) {
 				e.printStackTrace();
-			}	
+			}
 		}
-			
-			
-			public void actionPerformed(ActionEvent e){
 
-				if(e.getSource() == aceptar){
-					if(count < temp.length){
+		
+		/**
+		 * Asigna propiedades a los botones
+		 * @param boton
+		 * @param image
+		 * @param lw
+		 * @param lh
+		 * @param ancho
+		 * @param alto
+		 */
+		public void botones(JButton boton,ImageIcon image,int lw,int lh, int ancho, int alto){
+			boton.setIcon(image);
+			boton.setBorderPainted(false);
+			boton.setBounds(lw, lh, ancho, alto);
+			add(boton);
+			boton.addActionListener(this);
+		}
+		
+		
+		
+		public void actionPerformed(ActionEvent e) {
+
+			if (e.getSource() == aceptar) {
+				if (count < temp.length) {
 					fill();
 				}
-				
-				if(count == 15){
-				dispose();
-				}
-				
-				}
-				
+				if (count == 15) dispose();
 			}
-			/**
-			 * fill() recolecta los numeros enteros a ordenar , almacena en temporal
-			 * @return Devuelve el array de enteros a procesar por la clase Proceso
-			 */
-			private void fill (){
-			
-				if(count < temp.length){
-					boolean exception = true;
-				
-					if(!(input1.getText().equals(""))){
-					String s1 = input1.getText();
-					try{
-					temp[count] = Integer.parseInt(s1);	
-					}catch(NumberFormatException e){
-				
-					JOptionPane.showMessageDialog(null, "Solo numeros !");
-			exception = false;
-					}
-					if(exception){
-					System.out.println("valor [" + count +"] :" + temp[count]);
-					count++;
-					input1.setText("");
-					}
-					else{
-						exception = false;
-					}
-					}
-			}
-			}
-			
-			
-			
-			protected void paintComponent(Graphics g){
 
-				super.paintComponent(g);
-				g.drawImage(popup,1,1,null);		
-				
+			if(e.getSource() == cerrar){
+				dispose();
 			}
-			
-			
 			
 			
 		}
-	
-	
-	
-}
 
+		/**
+		 * fill() recolecta los numeros enteros a ordenar , almacena en temporal
+		 * 
+		 * @return Devuelve el array de enteros a procesar por la clase Proceso
+		 */
+		private void fill() {
+
+			if (count < temp.length) {
+				boolean exception = true;
+
+				if (!(input1.getText().equals(""))) {
+					String s1 = input1.getText();
+					try {
+						temp[count] = Integer.parseInt(s1);
+					} catch (NumberFormatException e) {
+
+						JOptionPane.showMessageDialog(null, "Solo numeros !");
+						input1.setText("");
+						exception = false;
+					}
+					if (exception) {
+						System.out.println("valor [" + count + "] :"
+								+ temp[count]);
+						count++;
+						input1.setText("");
+					} 
+				}
+			}
+		}
+
+		protected void paintComponent(Graphics g) {
+
+			super.paintComponent(g);
+			g.drawImage(popup, 1, 1, null);
+
+		}
+
+	}
+
+}
